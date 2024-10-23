652873e63f89055331c7b88521e2ad9172fb68f6
ALLAN OLIVEIRA BALIEIRO <allan.4951@aluno.pr.senac.br>
2024-10-23 15:21:38 -0300

    atualizaçao alteracao usuario,venda e backend de cidade

--- usuario/inserir.php
+++ usuario/inserir.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 include '../conexao.php';
 
@@ -16,23 +16,4 @@ $sql = "INSERT INTO usuario(nome,cpf,senha) VALUES('$nome','$cpf','$senha')";
 
   header("location: ../principal.php");
 
-=======
-<?php
-include '../conexao.php';
-
-//recebendo os dados do front-end
-$nome = $_REQUEST['nome'];
-$cpf = $_REQUEST['cpf'];
-$senha = $_REQUEST['senha'];
-
-echo " testando: $nome $cpf $senha";
-
-
-$sql = "INSERT INTO usuario(nome,cpf,senha) VALUES('$nome','$cpf','$senha')";
-  //EXECUTAR CODIGO SQL
-  $resultado = mysqli_query($conexao,$sql);
-
-  header("location: ../principal.php");
-
->>>>>>> 9ab13b928b2436c97629773f17a00065b75e35cc
 ?>
--- validacao.php
+++ validacao.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 <?php
 //iniciar sessão
@@ -15,22 +14,5 @@ if(!isset($_SESSION["cpf"]) or !isset($_SESSION['senha'])){
     //manda para o login
     header('location: index.php');
 }
-=======
 
-<?php
-//iniciar sessão
-session_start();
-
-// se não existe variavel de sessão CPF ou SENHA
-if(!isset($_SESSION["cpf"]) or !isset($_SESSION['senha'])){
-    //destruir a sessão
-    session_destroy();
-
-    //limpar variáveis de sessão
-    unset($_SESSION['cpf']);
-    unset($_SESSION['senha']);
-    //manda para o login
-    header('location: index.php');
-}
->>>>>>> 9ab13b928b2436c97629773f17a00065b75e35cc
 ?>
